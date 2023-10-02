7212cb539dee8d74628cd359ad88915d70a340ec
magaligiles <magalibelen.giles@gmail.com>
2023-10-02 18:34:37 -0300

    arreglo de while

--- main.js
+++ main.js
@@ -3,46 +3,47 @@ let montoDolarBlue = 2;
 let montoPesos = 3;
 let continuar = true;
 
-while (continuar){
-let opcionesConvertir = prompt("Hola! Bienvenido, ¿Qué operación deseas realizar?\n\n1. Convertir Dólar Oficial a Pesos\n2. Convertir Dólar Blue a Pesos\n3. Convertir Pesos a Dólares");
+while (continuar) {
+    let opcionesConvertir = prompt("Hola! Bienvenido, ¿Qué operación deseas realizar?\n\n1. Convertir Dólar Oficial a Pesos\n2. Convertir Dólar Blue a Pesos\n3. Convertir Pesos a Dólares");
 
-if (opcionesConvertir == "1") {
-    let montoDolarOficial = parseFloat(prompt("Ingrese el monto en Dólar Oficial:"));
-    if (!isNaN(montoDolarOficial)) {
-        let tipoDeCambioDolarOficial = 347;
-        let montoPesos = montoDolarOficial * tipoDeCambioDolarOficial;
-        alert(`${montoDolarOficial} Dólares Oficiales son aproximadamente ${montoPesos.toFixed(2)} Pesos.`);
-    } else {
-        alert("Por favor, ingrese un monto válido en Dólar Oficial.");
+    if (opcionesConvertir == "1") {
+        montoDolarOficial = parseFloat(prompt("Ingrese el monto en Dólar Oficial:"));
+        if (!isNaN(montoDolarOficial)) {
+            let tipoDeCambioDolarOficial = 347;
+            montoPesos = montoDolarOficial * tipoDeCambioDolarOficial;
+            alert(`${montoDolarOficial} Dólares Oficiales son aproximadamente ${montoPesos.toFixed(2)} Pesos.`);
+        } else {
+            alert("Por favor, ingrese un monto válido en Dólar Oficial.");
+        }
     }
-}
 
-if (opcionesConvertir == "2") {
-    let montoDolarBlue = parseFloat(prompt("Ingrese el monto en Dólar Blue:"));
-    if (!isNaN(montoDolarBlue)) {
-        let tipoDeCambioDolarBlue = 790; 
-        let montoPesos = montoDolarBlue * tipoDeCambioDolarBlue;
-        alert(`${montoDolarBlue} Dólares Blue son aproximadamente ${montoPesos.toFixed(2)} Pesos.`);
-    } else {
-        alert("Por favor, ingrese un monto válido en Dólar Blue.");
+    if (opcionesConvertir == "2") {
+        montoDolarBlue = parseFloat(prompt("Ingrese el monto en Dólar Blue:"));
+        if (!isNaN(montoDolarBlue)) {
+            let tipoDeCambioDolarBlue = 790;
+            montoPesos = montoDolarBlue * tipoDeCambioDolarBlue;
+            alert(`${montoDolarBlue} Dólares Blue son aproximadamente ${montoPesos.toFixed(2)} Pesos.`);
+        } else {
+            alert("Por favor, ingrese un monto válido en Dólar Blue.");
+        }
     }
-}
 
-if (opcionesConvertir == "3") {
-    let montoPesos = parseFloat(prompt("Ingrese el monto en Pesos:"));
-    if (!isNaN(montoPesos)) {
-        let tipoDeCambioDolarOficial = 100; 
-        let montoDolarOficial = montoPesos / tipoDeCambioDolarOficial;
-        alert(`${montoPesos} Pesos son aproximadamente ${montoDolarOficial.toFixed(2)} Dólares Oficiales.`);
-    } else {
-        alert("Por favor, ingrese un monto válido en Pesos.");
+    if (opcionesConvertir == "3") {
+        montoPesos = parseFloat(prompt("Ingrese el monto en Pesos:"));
+        if (!isNaN(montoPesos)) {
+            let tipoDeCambioDolarOficial = 100;
+            let montoDolarOficial = montoPesos / tipoDeCambioDolarOficial;
+            alert(`${montoPesos} Pesos son aproximadamente ${montoDolarOficial.toFixed(2)} Dólares Oficiales.`);
+        } else {
+            alert("Por favor, ingrese un monto válido en Pesos.");
+        }
     }
-}
 
-let respuesta = prompt("¿Desea realizar otra conversión? (si/no)");
-if (respuesta.toUpperCase() !== "si") {
-    continuar = false;
-}
+    respuesta = prompt("¿Desea realizar otra conversión? (si/no)");
+
+    if (respuesta.toLowerCase() !== "si") {
+        continuar = false;
+    }
 }
 
 alert("¡Gracias por usar el conversor!");
